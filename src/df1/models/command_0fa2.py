add557931d94d80b9b34e64012bb8ae01d441cb0
Jerther <jerther@gmail.com>
2018-08-29 10:02:42 -0400

    [FIX] a2 and aa commands frames proper implementation.

--- src/df1/models/command_0fa2.py
+++ src/df1/models/command_0fa2.py
@@ -5,10 +5,10 @@ from .file_type import FileType
 class Command0FA2(BaseCommand):
     """
     protected typed logical read with three address fields
-    http://www.plctalk.net/qanda/showthread.php?t=1016
+    Official doc 7-17
     """
-    def init_with_params(self, *, bytes_to_read, table, file_type: FileType, start, **kwargs):
-        sanitized_start = self._swap_endian(start)
-        data = [bytes_to_read, table, file_type.value]
-        data.extend(sanitized_start)
+    def init_with_params(self, *, bytes_to_read, table, file_type: FileType, start, start_sub=0x0, **kwargs):
+        if start > 0xfe or start_sub > 0xfe:  # pragma: nocover
+            raise NotImplementedError("Start index and start sub element higher than 0xfe not supported yet.")
+        data = [bytes_to_read, table, file_type.value, start, start_sub]
         super().init_with_params(cmd=0x0f, fnc=0xa2, command_data=data, **kwargs)
--- src/df1/models/command_0faa.py
+++ src/df1/models/command_0faa.py
@@ -5,17 +5,15 @@ from .file_type import FileType
 class Command0FAA(BaseCommand):
     """
     protected typed logical write with three address fields
-    http://www.plctalk.net/qanda/showthread.php?t=1016
+    Official doc 7-18
     """
-    def init_with_params(self, *, table, file_type: FileType, start, data_to_write, **kwargs):
+    def init_with_params(self, *, table, file_type: FileType, start, data_to_write, start_sub=0x0, **kwargs):
         if file_type == FileType.INTEGER:
             bytes_to_write = [b for i in data_to_write for b in self._swap_endian(i)]
         else:  # pragma: nocover
             raise NotImplementedError()
-        sanitized_start = self._swap_endian(start)
-        # convert data to write to list of bytes (si integer. supporter uniquement integer pour linstant.)
-        # prendre len de ca pour bytes to write
-        data = [len(bytes_to_write), table, file_type.value]
-        data.extend(sanitized_start)
+        if start > 0xfe or start_sub > 0xfe:  # pragma: nocover
+            raise NotImplementedError("Start index and start sub element higher than 0xfe not supported yet.")
+        data = [len(bytes_to_write), table, file_type.value, start, start_sub]
         data.extend(bytes_to_write)
         super().init_with_params(cmd=0x0f, fnc=0xaa, command_data=data, **kwargs)
